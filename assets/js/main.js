93b7234e025cb0eea5a09dfe94f5120b25926462
DiogoKramel <diogo.kramel@gmail.com>
2019-05-15 17:49:17 +0200

    First commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -357,7 +357,41 @@ $(document).ready(function($) {
         Contact Form
     =============================================== */
 
-    
+    $('#contactform').submit(function(){
+
+        var action = $(this).attr('action');
+
+        $("#alert").slideUp(750,function() {
+            $('#alert').hide();
+
+        $('#submit')
+            .after('<img src="assets/images/loading.gif" class="contactloader" />')
+            .attr('disabled','disabled');
+
+        $.post(action, {
+            name: $('#name').val(),
+            email: $('#email').val(),
+            message: $('#message').val()
+        },
+            function(data){
+                document.getElementById('alert').innerHTML = data;
+                $('#alert').slideDown('slow');
+                $('#contactform img.contactloader').fadeOut('slow',function(){$(this).remove();});
+                $('#submit').removeAttr('disabled');
+                if(data.match('success') !== null) {
+                    $('#name').val('');
+                    $('#email').val('');
+                    $('#message').val('');
+                }
+            }
+        );
+
+        });
+
+        return false;
+
+    });
+
 
     /* ==============================================
         Responsive video
