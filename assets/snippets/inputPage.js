97676fed4c8cb6382045927485a56af71ea862c3
DiogoKramel <diogo.kramel@gmail.com>
2019-05-16 12:04:35 +0200

    First commit

--- assets/snippets/inputPage.js
+++ assets/snippets/inputPage.js
@@ -136,3 +136,126 @@ $(document).ready(function(){
     $("#btn7").addClass('button-clicked-whale');
   });
 });
+
+function myFunction1() {
+  var popup = document.getElementById("myPopup1");
+  popup.classList.toggle("show");
+};
+
+function myFunction2() {
+  var popup = document.getElementById("myPopup2");
+  popup.classList.toggle("show");
+};
+
+function myFunction3() {
+  var popup = document.getElementById("myPopup3");
+  popup.classList.toggle("show");
+};
+
+function myFunction4() {
+  var popup = document.getElementById("myPopup4");
+  popup.classList.toggle("show");
+};
+
+function myFunction5() {
+  var popup = document.getElementById("myPopup5");
+  popup.classList.toggle("show");
+};
+
+function myFunction6() {
+  var popup = document.getElementById("myPopup6");
+  popup.classList.toggle("show");
+};
+
+function myFunction7() {
+  var popup = document.getElementById("myPopup7");
+  popup.classList.toggle("show");
+};
+
+function myFunction8() {
+  var popup = document.getElementById("myPopup8");
+  popup.classList.toggle("show");
+};
+
+function myFunction9() {
+  var popup = document.getElementById("myPopup9");
+  popup.classList.toggle("show");
+};
+
+function myFunction10() {
+  var popup = document.getElementById("myPopup10");
+  popup.classList.toggle("show");
+};
+
+function myFunction11() {
+  var popup = document.getElementById("myPopup11");
+  popup.classList.toggle("show");
+};
+
+function myFunction12() {
+  var popup = document.getElementById("myPopup12");
+  popup.classList.toggle("show");
+};
+
+function myFunction13() {
+  var popup = document.getElementById("myPopup13");
+  popup.classList.toggle("show");
+};
+
+function myFunction15() {
+  var popup = document.getElementById("myPopup15");
+  popup.classList.toggle("show");
+};
+
+var speciesSelected = [];
+function dataSpermWhale() {
+  document.getElementById("ageLifeExpectancy").value = "70"
+  document.getElementById("ageMaturationMin").value = "9"
+  document.getElementById("ageMaturationMax").value = "10"
+  document.getElementById("probBirth").value = "25"
+  localStorage.setItem("speciesSelected", "Sperm Whale")
+};
+
+function dataBlueWhale() {
+  document.getElementById("ageLifeExpectancy").value = "85"
+  document.getElementById("ageMaturationMin").value = "5"
+  document.getElementById("ageMaturationMax").value = "10"
+  document.getElementById("probBirth").value = "40"
+  localStorage.setItem("speciesSelected", "Blue Whale")
+};
+
+function dataFinWhale() {
+  document.getElementById("ageLifeExpectancy").value = "90"
+  document.getElementById("ageMaturationMin").value = "7"
+  document.getElementById("ageMaturationMax").value = "12"
+  document.getElementById("probBirth").value = "40"
+  localStorage.setItem("speciesSelected", "Fin Whale")
+};
+
+function dataRightWhale() {
+  document.getElementById("ageLifeExpectancy").value = "60"
+  document.getElementById("ageMaturationMin").value = "5"
+  document.getElementById("ageMaturationMax").value = "21"
+  document.getElementById("probBirth").value = "40"
+  localStorage.setItem("speciesSelected", "Right Whale")
+};
+
+function dataHumpbackWhale() {
+  document.getElementById("ageLifeExpectancy").value = "50"
+  document.getElementById("ageMaturationMin").value = "5"
+  document.getElementById("ageMaturationMax").value = "7"
+  document.getElementById("probBirth").value = "33"
+  localStorage.setItem("speciesSelected", "Humpback Whale")
+};
+
+function dataGrayWhale() {
+  document.getElementById("ageLifeExpectancy").value = "60"
+  document.getElementById("ageMaturationMin").value = "5"
+  document.getElementById("ageMaturationMax").value = "11"
+  document.getElementById("probBirth").value = "40"
+  localStorage.setItem("speciesSelected", "Gray Whale")
+};
+
+function dataAlternativeWhale() {
+  localStorage.setItem("speciesSelected", "Alternative Species")
+};
